9cb6363e828f2f0df15418c7d354766adfe74d45
datng <datnguyennguyen.2000@gmail.com>
2020-12-05 02:14:28 +0700

    hoan thanh co ban ve home

--- app/src/main/java/datnguyen/lab/passioproject/MainActivity.java
+++ app/src/main/java/datnguyen/lab/passioproject/MainActivity.java
@@ -3,12 +3,18 @@ package datnguyen.lab.passioproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvWelcome = findViewById(R.id.tv_welcome);
+        tvWelcome.setText("GeeksForGeeks(Firebase Authentication)");
     }
 }
